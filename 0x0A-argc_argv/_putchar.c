a50c1668cb5da8c9e4f32c9f931acff327929dad
Luvhengop <luvhengop@gmail.com>
2023-04-10 05:34:46 -0700

    program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: print character 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set.
+ */
+int _putchar(char c)
+{
+return (write(1, 7C, 1))
+}
