6866da315b6484dcd1fc0756b89af3ff4787dbcc
Luvhengop <luvhengop@gmail.com>
2023-04-21 02:42:26 -0700

    updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,7 +2,7 @@
 
 
 /**
-* is_prime_number - says if an integer is number 
+* is_prime_number - says if an integer is number
 * or not number
 * @n: integer number to evaluate
 *
@@ -17,7 +17,7 @@ int is_prime_number(int n)
 	}
 	else if (n > 2)
 	{
-		return (is_prime_number (2));
+	return (is_prime_number (2));
 	}
 	return (0);
 }
