a79a311dfac6e2b913c9e1824383dbc9d30948b6
Luvhengop <luvhengop@gmail.com>
2023-04-21 02:39:41 -0700

    updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,44 @@
 #include "main.h"
 
+
 /**
-* is_prime_number - recursively find if @n is a prime number
-* @n: integer number
+* is_prime_number - says if an integer is number 
+* or not number
+* @n: integer number to evaluate
 *
 *Return: an integer value for the sqrt, -1 if it failed to find natural sqrt
 */
+
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (actual_prime(n, n - 1));
+	if (n == 2)
+	{
+		return (1);
+	}
+	else if (n > 2)
+	{
+		return (is_prime_number (2));
+	}
+	return (0);
 }
 
 /**
-* actual_prime - calculates if a number is prime recursively
+* test_prime  - calculates if a number is prime recursively
 * @n: integer number
 * @j: composite tester
 *
 *Return: an integer
 */
 
-int actual_prime(int n, int j)
+int test_prime(int n, int j)
 {
-	if (j == 1)
-		return (1);
-	if (n % j == 0 && j > 0)
+	if (n % j == 0 && n != j)
+	{
 		return (0);
-	return (actual_prime(n, j - 1));
+	}
+	else if (n == j)
+	{
+		return (1);
+	}
+	return (test_prime(n, j + 1));
 }
