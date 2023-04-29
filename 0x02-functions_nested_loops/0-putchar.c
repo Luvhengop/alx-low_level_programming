461a180860521604e527b6d3f90510a66d580c8a
Luvhengop <luvhengop@gmail.com>
2023-04-28 23:34:50 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,23 @@
  * main - print _putchar
  * is written by bwave
  * Return: 0
+ *
  */
 
 int main(void)
 {
-putchar('-');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	/*
+	 * use _putchar() to output a formatted array of chars onto standard out and
+	 * return 0 after that task has been accomplished
+	 */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
